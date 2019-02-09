5694fb4d6345111cb53812a199835e92e3da57b9
Gabriel Nasr <nasr.gab@gmail.com>
2019-02-09 17:39:10 -0300

    Move collector function outside the main and code formatting

--- main.go
+++ main.go
@@ -1,77 +1,74 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
-    "net/http"
-    "flag"
-    "github.com/prometheus/client_golang/prometheus"
-    "github.com/prometheus/client_golang/prometheus/promhttp"
-	"github.com/d2r2/go-dht"
-    logger "github.com/d2r2/go-logger"
+	"net/http"
+
+	dht "github.com/d2r2/go-dht"
+	logger "github.com/d2r2/go-logger"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 const sensorType = dht.DHT22
 const gpioPort = 4
-var (
-        addr = flag.String("listen-address", ":8090", "The address to listen on for HTTP requests.")
-    )
+
+func getStats() (temp, hum float64) {
+	var (
+		temperature, humidity float32
+		err                   error
+	)
+
+	temperature, humidity, err = dht.ReadDHTxx(sensorType, gpioPort, true)
+
+	if err != nil {
+		log.Print(err)
+	}
+
+	if humidity == 0 {
+		log.Print(err)
+	}
+
+	return float64(temperature), float64(humidity)
+}
 
 func main() {
-    flag.Parse()
+	addr := flag.String("listen-address", ":8090", "The address to listen on for HTTP requests.")
 
-    logger.ChangePackageLogLevel("dht", logger.ErrorLevel)
+	flag.Parse()
 
-    if err := prometheus.Register(prometheus.NewGaugeFunc(
-        prometheus.GaugeOpts{
-            Subsystem: "dht22",
-            Name:      "temperature_celsius",
-            Help:      "Temperature in Celsius",
-        },
-        func() float64 {
-            var (
-                temperature, humidity float32
-                err error
-            )
-	        temperature, humidity, err = dht.ReadDHTxx(sensorType, gpioPort, true)
-            if err != nil {
-                log.Print(err)
-            }
-            if humidity == 0 {
-                log.Print(err)
-            }
-            return float64(temperature)
-        },
-    )); err == nil {
-        fmt.Println("GaugeFunc 'temperature_celsius', registered.")
-    }
+	logger.ChangePackageLogLevel("dht", logger.ErrorLevel)
 
-    if err := prometheus.Register(prometheus.NewGaugeFunc(
-        prometheus.GaugeOpts{
-            Subsystem: "dht22",
-            Name:      "humidity_percent",
-            Help:      "Humidity in percent",
-        },
-        func() float64 {
-            var (
-                temperature, humidity float32
-                err error
-            )
-	        temperature, humidity, err = dht.ReadDHTxx(sensorType, gpioPort, true)
-            if err != nil {
-                log.Print(err)
-            }
-            if temperature == 0 {
-                log.Print(err)
-            }
-            return float64(humidity)
-        },
-    )); err == nil {
-        fmt.Println("GaugeFunc 'humidity_percent', registered.")
-    }
+	if err := prometheus.Register(prometheus.NewGaugeFunc(
+		prometheus.GaugeOpts{
+			Subsystem: "dht22",
+			Name:      "temperature_celsius",
+			Help:      "Temperature in Celsius",
+		},
+		func() float64 {
+			temperature, _ := getStats()
+			return temperature
+		},
+	)); err == nil {
+		fmt.Println("GaugeFunc 'temperature_celsius', registered.")
+	}
 
+	if err := prometheus.Register(prometheus.NewGaugeFunc(
+		prometheus.GaugeOpts{
+			Subsystem: "dht22",
+			Name:      "humidity_percent",
+			Help:      "Humidity in percent",
+		},
+		func() float64 {
+			_, humidity := getStats()
+			return humidity
+		},
+	)); err == nil {
+		fmt.Println("GaugeFunc 'humidity_percent', registered.")
+	}
 
-
-    http.Handle("/metrics", promhttp.Handler())
-    log.Fatal(http.ListenAndServe(*addr, nil))
+	http.Handle("/metrics", promhttp.Handler())
+	log.Fatal(http.ListenAndServe(*addr, nil))
 }
