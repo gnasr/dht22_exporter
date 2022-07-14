1a8efb98352557671cdd019d20133a0bd0d92ec8
Trinigv <99trini@gmail.com>
2022-07-14 15:59:45 -0300

    New js file

---
+++ MIAMOR.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>HTML 5 Boilerplate</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+	<script src="index.js"></script>
+    <video autoPlay muted loop> <source src='https://player.vimeo.com/external/513400016.hd.mp4?s=00ae29f0328404aab9079b38439b83a6fcb746dd&profile_id=174&oauth2_token_id=57447761'/> </video>
+  </body>
+</html>
